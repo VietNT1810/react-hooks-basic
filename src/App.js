f888a0b3d2f01b5ee30857cb469014e28adb7d26
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-04-17 22:03:01 +0700

    add custom hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import queryString from 'query-string'
+import Pagination from './components/Pagination';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import PostFiltersForm from './components/PostFiltersForm';
+import Clock from './components/Clock';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -8,6 +14,32 @@ function App() {
     { id: 2, title: 'Todo 2' },
     { id: 3, title: 'Todo 3' },
   ]);
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1
+  });
+  const [filters, setFilters] = useState({
+    _page: 1,
+    _limit: 10,
+    title_like: '',
+  })
+
+  useEffect(() => {
+    const paramsString = queryString.stringify(filters);
+    console.log(paramsString);
+    async function getData() {
+      await axios.get(`http://js-post-api.herokuapp.com/api/posts?${paramsString}`)
+        .then(res => {
+          setPostList(res.data.data);
+          setPagination(res.data.pagination);
+        })
+        .catch(err => { throw err; });
+    }
+
+    getData();
+  }, [filters])
 
   const handleTodoClick = (todo) => {
     console.log(todo);
@@ -30,10 +62,37 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  const handlePageChange = (newPage) => {
+    console.log('New page:', newPage);
+    setFilters({
+      ...filters,
+      _page: newPage
+    })
+  }
+
+  const handleFiltersChange = (newFilters) => {
+    console.log("New Filter:", newFilters);
+    setFilters({
+      ...filters,
+      _page: 1,
+      title_like: newFilters.searchTerm
+    })
+  }
+
   return (
     <div className="App">
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h1>React hooks - Clock</h1>
+
+      <Clock />
+
+      {/* <PostFiltersForm onSubmit={handleFiltersChange} />
+      <PostList posts={postList} /> */}
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+      {/* <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      /> */}
     </div>
   );
 }
---
+++ src/components/Clock/index.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import useClock from '../../hooks/useClock';
+
+Clock.propTypes = {};
+
+function Clock() {
+    const { timeString } = useClock();
+    return (
+        <p style={{ fontSize: "45px" }}>{timeString}</p>
+    );
+}
+
+export default Clock;
